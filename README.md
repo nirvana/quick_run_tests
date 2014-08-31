7d92e36ab8287d805620c0dca128e2fef62417d0
Nirvana <hello@nirvana.io>
2014-08-31 13:30:48 -0500

    Initial work

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-QRT
-===
+# quick_run_tests  Quick! Run some Tests!
 
-Test  your REST APIs with Quick Rest Test
+A very simple project to quickly generate a simple load test, and collect statistics.
+
+It runs a series of tests called iterations. An iteration is a number of simultaneas events. EG: they
+could be requests to a webserver.  If your iterations are 1, 5, 10, 15 & 25, then the test effectively
+sees what happens when 1, 5, 10, 15 and 25 people try to hit the webserver at the same time.
+
+The actual test function is something you supply so it could be anything. It just needs to run the test,
+collect the time for that indivual test and indicate whether it failed or not.
+
+It returns a map in the form of:  %{time: nil, success: false}
+
+For more sophisticated testing, we support passing a long a set of paths. These paths can be scenarios
+or sample data for your tests.
+
+The end result is a csv with stats for each load level.
+  - Current stats:
+    - Average time per request
+    - %successful requests
+
+At each load level the tests are run in parallel
+
+## HowTo
+
+  - Include quick_rest_test as a dependency
+  - Configure your scenario
+  - Qrt.run(scenario)
+
+### Scenarios
+
+  - Empty: Qrt.scenario()
+  - Scenarios are maps with:
+    - iterations_list: list of number of iterations to do, eg: [1, 10, 100]
+    - paths: paths of documents with test data (optional)
+    - rest_time: a pause between each load level (in milliseconds)
+    - f_test/1: test function: eg: an WEB Request. input is paths, output is %{time: nil, success: false}
---
+++ lib/qrt.ex
@@ -0,0 +1,72 @@
+defmodule Qrt do
+
+  def scenario do
+    %{iterations: nil, paths: nil, rest_time: 10_000, f_test: nil}
+  end
+
+  def results do
+    %{num: 0, avg_time: 0, percent_success: 0}
+  end
+
+  def accumulator do
+    %{cummulative_time: 0, success: 0, count: 0}
+  end
+
+  def test do
+    %{time: nil, success: false}
+  end
+
+  # in: Map that configures the test
+  # out: csv file with results
+  def run(scenario) do
+    {date, _} = System.cmd("date", ["+%Y-%m-%d_%H-%M-%S"])
+    output = File.open! "results-#{String.strip(date, "\n")}.csv", [:utf8, :write]
+    # List of tests, value is mumber of simultaneous users in each test
+    # Tasks simulate N users hitting API at the same time.
+    scenario[:iterations]
+    |> Enum.map(fn(num) ->
+      IO.puts "== running #{num} iterations"
+      :timer.sleep(scenario[:rest_time]) #wait before next run
+      run_test(num, scenario)
+    end)
+    |> Enum.map(fn(results) ->
+      IO.puts output, "#{results[:num]}, #{results[:avg_time]}, #{results[:percent_success]}"
+    end)
+  end
+
+  # Do test, does n calls of test function, returns results struct (one line of the csv)
+  # in: Number of iterations in this test, & config map
+  # out: results map representing one line in the csv file
+  def run_test(num, scenario) do
+    Enum.map(1..num, fn(x)->
+      IO.puts ".. spawn #{x} of #{num} tests"
+      Task.async(fn->
+        &f_test(scenario)
+      end)
+    end)
+    |> Enum.map(&Task.await/1)
+    |> Map.flatten
+    |> Enum.reduce(accumulator, fn(test, acc)-> #accumulates the results
+      success = cond do
+        test[:success] -> acc[:success] + 1 #increment counter
+        true -> acc[:success] #don't increment
+      end
+      %{acc |
+        count: acc[:count]+1,
+        success: success,
+        time: acc[:time] + test[:time]
+        }
+    end)
+    |> stats(num)
+  end
+
+  def stats(acc, num) do
+    ${results |
+      num: num,
+      avg_time: acc[:cummulative_time] / acc[:count],
+      percent_success: acc[:success] / acc[:count]
+    }
+  end
+
+
+end
